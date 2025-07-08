f861fa3980ced06943429cdb4bb414434840ed01
NicramDev <nc.nicram@hotmail.com>
2025-07-08 10:16:27 +0200

    Update version.ts

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,12 @@
-export const VERSION = "BETA 1.0";
+export const VERSION = "BETA 1.0.0";
 export const CHANGELOG = `
+CHANGELOG 08.07.2025
+
 Dodano:
+- changelog - pokazuje co zmienione
+
+Naprawiono:
+- błędy związane z bazą danych
+- zapisywanie zdjęć w bazie danych
 
 `;
