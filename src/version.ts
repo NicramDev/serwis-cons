a90f4f3b74b60053065f8ca08f582da223cf1e58
NicramDev <nc.nicram@hotmail.com>
2025-07-08 14:28:53 +0200

    Update version.ts

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = "BETA 1.0.1";
+export const VERSION = "                                                                              BETA 1.0.1";
 export const CHANGELOG = `
 Dodano:
 - alfabetyczne ustawienie urządzeń w pojazdach
