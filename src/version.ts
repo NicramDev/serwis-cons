4b99c8f96e0ba3afd8fb4596e5c097587db749ae
NicramDev <nc.nicram@hotmail.com>
2025-10-29 12:42:56 +0100

    Change version to BETA 1.15.0
    Updated the version number to BETA 1.15.0.

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,4 @@
-export const VERSION = "                                                                              BETA 1.0.1";
+export const VERSION = "                                                                              BETA 1.15.0";
 export const CHANGELOG = `
-Dodano:
-- alfabetyczne ustawienie urządzeń w pojazdach
+Nowa wersja + nowe funckje + wiele wiecej
 `;
